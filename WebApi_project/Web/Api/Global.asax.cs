8ad9e226550d1522a9a354dd6949740c341cadd5
ADAM-LENOVO\Adam-245594 <adamsgrzonkowski@gmail.com>
2018-10-23 10:14:28 +0200

    Global error handler added.

--- WebApi_project/Web/Api/Global.asax.cs
+++ WebApi_project/Web/Api/Global.asax.cs
@@ -1,18 +1,36 @@
 ﻿using Api.App_Start;
+using log4net;
 using System;
 using System.IO;
+using System.Web;
 using System.Web.Http;
 
 namespace Api
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(WebApiApplication));
+
+        /// <summary>
+        /// Entry point for application.
+        /// </summary>
         protected void Application_Start()
         {
             log4net.Config.XmlConfigurator.Configure(new FileInfo(AppDomain.CurrentDomain.SetupInformation
                 .ConfigurationFile));
             GlobalConfiguration.Configure(WebApiConfig.Register);
             ContainerConfig.Configure();
+        }
+
+        /// <summary>
+        /// Global error handler (unhandled exceptions will come through here).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = HttpContext.Current.Server.GetLastError();
+            _logger.Error(ex);
         }
     }
 }
